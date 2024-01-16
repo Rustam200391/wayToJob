e3a87f42090da8eae841347ccde9be5404510d20
Rustam Guseinov <rg20031991.com>
2024-01-16 19:26:26 +0300

    The next step to build this app

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 let xRay = 0;
 
 let destination = prompt("Введите название места назначения");
-let wayToDestination = prompt("Введите расстояние до места"`${destination}`);
+let wayToDestination = prompt(
+  "Введите расстояние в км до " + " " + `${destination}`
+);
 let duration = prompt(
-  "Введите время за которое вам нужно преодолеть"`${wayToDestination}`,
+  "Введите количество часов за которое вам нужно преодолеть" +
+    " " +
+    `${wayToDestination}` +
+    " км ",
   "что бы прибыть в" + `${destination}`
+);
+
+let speedToDriveRounded = wayToDestination / duration;
+let speedToDrive = Math.round(speedToDriveRounded);
+// округляем
+alert(
+  "До" +
+    " " +
+    `${destination}` +
+    " " +
+    "вы будете ехать со скоростью" +
+    " " +
+    `${speedToDrive}` +
+    " " +
+    "км/ч"
 );
